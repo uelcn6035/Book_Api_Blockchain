16dec958b4f0b20bfcbb13a1819f539e968fb22d
Jeleel-M <120250699+Jeleel-M@users.noreply.github.com>
2024-04-27 11:37:13 +0000

    version 1.0

--- book_api/dist/src/routes/bookRoute.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { getBookData } from "../controllers/bookController.js";
-import { validateBookTitle } from "../middleware/validators.js";
-const router = express.Router();
-router.get("/:title", validateBookTitle, getBookData);
-export default router;
-//# sourceMappingURL=bookRoute.js.map
