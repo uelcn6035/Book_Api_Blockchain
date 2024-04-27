ae78b57e546a25286cda29721f6ad753613e20b5
Jeleel-M <120250699+Jeleel-M@users.noreply.github.com>
2024-04-27 13:31:06 +0000

    integration

--- book_api/dist/src/middleware/validators.js
+++
@@ -1,6 +0,0 @@
-import { param } from "express-validator";
-export const validateBookTitle = param("title")
-    .isString()
-    .notEmpty()
-    .withMessage("Book title must be a non-empty string");
-//# sourceMappingURL=validators.js.map
